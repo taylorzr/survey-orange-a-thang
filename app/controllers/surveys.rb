50541bb83dc416a66d0dbe9131a992d4d7bf881e
Ricky Do <ricky.dop@gmail.com>
2014-09-06 14:30:21 -0500

    Adding updated views and controllers

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -11,6 +11,15 @@ get '/surveys/new' do
   end
 end
 
+get 'surveys/:id/info' do
+  @survey = Survey.find(params[:id])
+  if @survey.creator.id == current_user
+    erb :"surveys/info"
+  else
+    redirect '/surveys'
+  end
+end
+
 get '/surveys/:id' do |id|
   @survey = Survey.find(id)
   erb :"surveys/show"
@@ -18,15 +27,9 @@ end
 
 post '/surveys/:id' do |id|
   survey = Survey.find(id)
-  # raise params.inspect
-
   params[:questions].each do |question_id, choice_id|
     Answer.create(question_id: question_id, choice_id: choice_id)
   end
-
-  # survey.questions.each do |question|
-  #   answer = Answer.create(question_id: question.id, choice_id: params[choice.id])
-  # end
   redirect '/surveys'
 end
 
@@ -47,7 +50,7 @@ post '/surveys' do
   end
 end
 
-get '/questions/new' do 
+get '/questions/new' do
   if request.xhr?
     erb :"surveys/_question", layout: false
   end
